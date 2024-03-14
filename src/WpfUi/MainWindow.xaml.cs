711dd354b2b53fb1f3c52f523197276937ef7f0b
Sawyron <s4pakvv@gmail.com>
2024-03-15 01:01:17 +0700

    Add project files.

---
+++ src/WpfUi/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WpfUi;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        MessageBox.Show(
+            $"{e.Source}{Environment.NewLine}{e.OriginalSource}",
+            "Canvas clicked",
+            MessageBoxButton.OK,
+            MessageBoxImage.Information);
+    }
+}
