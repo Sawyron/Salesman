d9744ea639de80f3202a9a9bd41495be85ad6ea3
Sawyron <s4pakvv@gmail.com>
2024-04-05 01:20:48 +0700

    refactor graph to improve readability

--- src/Salesman.Domain/Graph/Graph.cs
+++ src/Salesman.Domain/Graph/Graph.cs
@@ -1,20 +1,21 @@
 ﻿namespace Salesman.Domain.Graph;
 
-public class Graph<N, V> where N : notnull
+public class Graph<TNode, TValue> where TNode : notnull
 {
-    private readonly List<N> _nodes;
-    private readonly Dictionary<N, IDictionary<N, V>> _adjacency;
+    private readonly List<TNode> _nodes;
+    private readonly Dictionary<TNode, IDictionary<TNode, TValue>> _adjacency;
 
-    public Graph(IEnumerable<N> nodes, IDictionary<N, IDictionary<N, V>> adjacency, bool directed = false)
+    public Graph(IEnumerable<TNode> nodes, IDictionary<TNode, IDictionary<TNode, TValue>> adjacency, bool directed = false)
     {
         _nodes = nodes.ToList();
         if (_nodes.Count != _nodes.Distinct().Count())
         {
             throw new ArgumentException("All nodes must be unique");
         }
-        Dictionary<N, IDictionary<N, V>> connections = nodes
-            .ToDictionary(n => n, n => new Dictionary<N, V>() as IDictionary<N, V>);
-        foreach ((N key, var _) in connections)
+        var connections = nodes.ToDictionary(
+            node => node,
+            _ => new Dictionary<TNode, TValue>() as IDictionary<TNode, TValue>);
+        foreach ((TNode key, var _) in connections)
         {
             if (!_nodes.Contains(key))
             {
@@ -27,9 +28,9 @@ public class Graph<N, V> where N : notnull
         }
         if (!directed)
         {
-            foreach (var (node, edges) in connections)
+            foreach ((TNode node, IDictionary<TNode, TValue> edges) in connections)
             {
-                foreach ((N adjacentNode, V value) in edges)
+                foreach ((TNode adjacentNode, TValue value) in edges)
                 {
                     if (!connections[adjacentNode].ContainsKey(node))
                     {
@@ -41,15 +42,24 @@ public class Graph<N, V> where N : notnull
         _adjacency = connections;
     }
 
-    public IReadOnlyList<N> Nodes => _nodes.AsReadOnly();
+    public IReadOnlyList<TNode> Nodes => _nodes.AsReadOnly();
 
-    public IReadOnlyDictionary<N, V> this[N node] =>
-        _adjacency.TryGetValue(node, out IDictionary<N, V>? edges) ?
+    public IReadOnlyDictionary<TNode, TValue> this[TNode node] =>
+        _adjacency.TryGetValue(node, out IDictionary<TNode, TValue>? edges) ?
             edges.AsReadOnly() : throw new ArgumentException($"Graph does not contain node '{node}'");
 
-    public V this[N node, N ajancentNode] =>
-        _adjacency.TryGetValue(node, out var edjes) ?
-            edjes.TryGetValue(ajancentNode, out var value) ?
-                value : throw new ArgumentException($"Graph does not contain node '{node}'")
-            : throw new ArgumentException($"Graph does not contain value '{node}' and {ajancentNode}");
+    public TValue this[TNode node, TNode ajancentNode]
+    {
+        get
+        {
+            if (_adjacency.TryGetValue(node, out var edges) && edges.TryGetValue(ajancentNode, out var value))
+            {
+                return value;
+            }
+            else
+            {
+                throw new ArgumentException($"There is no connection between '{node}' and '{ajancentNode}'");
+            }
+        }
+    }
 }
