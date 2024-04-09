5a42f57dfb879368b2986d149e25abd70a679046
Sawyron <s4pakvv@gmail.com>
2024-04-10 01:59:06 +0700

    implement returning the best found result in cancellation for exhaustive search

--- src/Salesman.Domain/Pathfinders/ExhausiveSearchSalesmanPathfinder.cs
+++ src/Salesman.Domain/Pathfinders/ExhausiveSearchSalesmanPathfinder.cs
@@ -14,16 +14,33 @@ public sealed class ExhaustiveSearchSalesmanPathfinder<TNode, TValue> : ISalesma
         {
             return new PathResult<TNode, TValue>([], TValue.Zero);
         }
-        var (path, length) = await Task.Run(() => nodes.Skip(1)
-            .Permutations()
-            .Select(c => c
-                .Prepend(nodes[0])
-                .Append(nodes[0])
-                .ToList())
-            .AsParallel()
-            .WithCancellation(cancellationToken)
-            .Select(p => (Path: p, Lenght: graph.CalculatePathLength(p)))
-            .MinBy(tuple => tuple.Lenght), cancellationToken);
+        var (path, length) = await Task.Run(() =>
+        {
+            var bestSolution = (
+                Path: nodes
+                    .Append(nodes[0])
+                    .ToList(),
+                Length: graph.CalculatePathLength(nodes.Append(nodes[0])));
+            var variants = nodes.Skip(1)
+                        .Permutations()
+                        .Select(c => c
+                            .Prepend(nodes[0])
+                            .Append(nodes[0])
+                            .ToList())
+                        .Select(p => (Path: p, Lenght: graph.CalculatePathLength(p)));
+            foreach (var variant in variants)
+            {
+                if (variant.Lenght < bestSolution.Length)
+                {
+                    bestSolution = variant;
+                }
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
+            }
+            return bestSolution;
+        }, cancellationToken);
         return new PathResult<TNode, TValue>(path, length);
     }
 }
