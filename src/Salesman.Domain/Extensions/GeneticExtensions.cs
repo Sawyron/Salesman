4b52cf9abbc3f05b5035c5f1a42cb8a2b5832516
Sawyron <s4pakvv@gmail.com>
2024-11-06 19:13:44 +0700

    implement Cross extension method

---
+++ src/Salesman.Domain/Extensions/GeneticExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Salesman.Domain.Extensions;
+
+public static class GeneticExtensions
+{
+    public static IList<T> Cross<T>(this IList<T> first, IList<T> second, int crossPoint)
+    {
+        var result = new List<T>(first.Count);
+        result.AddRange(first.Take(crossPoint + 1));
+        HashSet<T> addedElements = [.. result];
+        List<T> leftFirstElements = first.Skip(crossPoint + 1).ToList();
+        for (int i = crossPoint + 1; i < second.Count; i++)
+        {
+            T chosenElement = addedElements.Contains(second[i]) ?
+                leftFirstElements[0]
+                : second[i];
+            result.Add(chosenElement);
+            addedElements.Add(chosenElement);
+            leftFirstElements.Remove(chosenElement);
+        }
+        return result;
+    }
+}
---
+++ tests/Salesman.Domain.Tests/Extensions/GeneticExtensionsTest.cs
@@ -0,0 +1,30 @@
+﻿using Salesman.Domain.Extensions;
+
+namespace Salesman.Domain.Tests.Extensions;
+
+public class GeneticExtensionsTest
+{
+    [Fact]
+    public void TestCross()
+    {
+        List<int> first = [0, 3, 1, 4, 2];
+        List<int> second = [0, 2, 4, 3, 1];
+        List<int> expected = [0, 1, 4, 1, 2];
+        int crossingPoint = 1;
+
+        IList<int> actual = first.Cross(second, crossingPoint);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestCross2()
+    {
+        List<int> first = [0, 3, 1, 4, 2];
+        List<int> second = [0, 2, 4, 3, 1];
+        List<int> expected = [0, 2, 1, 4, 3];
+        int crossingPoint = 1;
+
+        IList<int> actual = second.Cross(first, crossingPoint);
+        Assert.Equal(expected, actual);
+    }
+}
